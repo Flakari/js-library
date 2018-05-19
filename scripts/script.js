45122ba9305f6dc9e58a2e1b6cc1aac1df3d9e7e
Nate Dimock <natedimock@yahoo.com>
2018-05-19 10:48:08 -0500

    Page can now read form data, pushing it to console log

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,6 @@
 let libraryTable = document.querySelector('#library-table');
+let formSubmit = document.querySelector('.form-submit');
+
 
 let myLibrary = [];
 
@@ -14,6 +16,21 @@ function addBookToLibrary(newBook) {
 function render() {
     
 }
+
+formSubmit.addEventListener('click', function(e) {
+    e.preventDefault();
+    let bookData = document.getElementsByTagName('input');
+    let consoleLog = ''
+    for (let i = 1; i < bookData.length - 1; i++) {
+        consoleLog += bookData[i].value + ' ';
+    }
+    consoleLog += bookData[bookData.length - 1].checked;
+    console.log(consoleLog);
+    for (let i = 1; i < bookData.length - 1; i++) {
+        bookData[i].value = '';
+    }
+    bookData[bookData.length - 1].checked = false;
+});
 
 let gameOfThrone = new Book('Game of Thrones', 'George R.R. Martin');
 addBookToLibrary(gameOfThrone);
