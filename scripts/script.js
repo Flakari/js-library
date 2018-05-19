d3d26c2831fbd0175f076693931c3c364cf15145
Nate Dimock <natedimock@yahoo.com>
2018-05-19 15:33:31 -0500

    Submit button adds new books using forms into library array

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,15 @@ let formSubmit = document.querySelector('.form-submit');
 
 let myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, pageCount, read) {
     this.title = title;
     this.author = author;
+    this.pageCount = pageCount;
+    this.read = read;
 }
 
 function addBookToLibrary(newBook) {
-    myLibrary.push(newBook);
+    myLibrary.unshift(newBook);
 }
 
 function render() {
@@ -18,21 +20,26 @@ function render() {
 }
 
 formSubmit.addEventListener('click', function(e) {
-    e.preventDefault();
     let bookData = document.getElementsByTagName('input');
-    let consoleLog = ''
-    for (let i = 1; i < bookData.length - 1; i++) {
-        consoleLog += bookData[i].value + ' ';
+    
+    if(bookData[3].checkValidity()) {
+        e.preventDefault();
+    } else {
+        return false;
     }
-    consoleLog += bookData[bookData.length - 1].checked;
-    console.log(consoleLog);
+    
+    newBook = new Book(bookData[1].value, bookData[2].value, bookData[3].value, bookData[4].checked);
+    addBookToLibrary(newBook);
+    console.log(myLibrary);
     for (let i = 1; i < bookData.length - 1; i++) {
         bookData[i].value = '';
     }
     bookData[bookData.length - 1].checked = false;
 });
 
-let gameOfThrone = new Book('Game of Thrones', 'George R.R. Martin');
-addBookToLibrary(gameOfThrone);
+let newBook = new Book('Game of Thrones', 'George R.R. Martin', 500, true);
+addBookToLibrary(newBook);
+newBook = new Book('Expendables', 'Slyvester Stallone', 300, true);
+addBookToLibrary(newBook);
 console.log(myLibrary);
-console.log(libraryTable);
+
